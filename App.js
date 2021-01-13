3ac72c1740e0efbe7f0915092060b19489e9668e
Hamilton <hamiltonvuu@gmail.com>
2021-01-12 22:08:52 -0600

    course list

--- App.js
+++ App.js
@@ -1,27 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, SafeAreaView } from "react-native";
+import Banner from "./Banner";
+import CourseList from "./CourseList";
 
 const schedule = {
   title: "CS Courses for 2018-2019",
+  courses: [
+    {
+      id: "F101",
+      title: "Computer Science: Concepts, Philosophy, and Connections",
+      meets: "MWF 11:00-11:50",
+    },
+    {
+      id: "F110",
+      title: "Intro Programming for non-majors",
+      meets: "MWF 10:00-10:50",
+    },
+    {
+      id: "F111",
+      title: "Fundamentals of Computer Programming I",
+      meets: "MWF 13:00-13:50",
+    },
+    {
+      id: "F211",
+      title: "Fundamentals of Computer Programming II",
+      meets: "TuTh 12:30-13:50",
+    },
+  ],
 };
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.bannerStyle}>{schedule.title}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Banner title={schedule.title} />
+      <CourseList courses={schedule.courses} />
+    </SafeAreaView>
   );
 };
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
     justifyContent: "center",
-  },
-  bannerStyle: {
-    color: "#888",
-    fontSize: 32,
+    paddingTop: 20,
   },
 });
 
