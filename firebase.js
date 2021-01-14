4137881ef5dbe7ba8499890b156a9751566b60a5
Hamilton <hamiltonvuu@gmail.com>
2021-01-14 17:39:31 -0600

    fetch database data

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import * as firebase from "firebase";
+
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDWzdF0jYdlTJFoR-JMpUsQy5zfyeKzeEI",
+  authDomain: "scheduler-b635c.firebaseapp.com",
+  databaseURL: "https://scheduler-b635c-default-rtdb.firebaseio.com",
+  projectId: "scheduler-b635c",
+  storageBucket: "scheduler-b635c.appspot.com",
+  messagingSenderId: "369357101330",
+  appId: "1:369357101330:web:2e5e0d2dbfee745a3abbcb",
+  measurementId: "G-JS9D11GEMZ",
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export { firebase };
--- screens/ScheduleScreen.js
+++ screens/ScheduleScreen.js
@@ -3,6 +3,12 @@ import { StyleSheet, SafeAreaView } from "react-native";
 import Banner from "../components/Banner";
 import CourseList from "../components/CourseList";
 import UserContext from "../UserContext";
+import { firebase } from "../firebase";
+
+const fixCourses = (json) => ({
+  ...json,
+  courses: Object.values(json.courses),
+});
 
 const ScheduleScreen = ({ navigation }) => {
   const user = useContext(UserContext);
@@ -15,16 +21,15 @@ const ScheduleScreen = ({ navigation }) => {
     });
   };
 
-  const url = "https://courses.cs.northwestern.edu/394/data/cs-courses.php";
-
   useEffect(() => {
-    const fetchSchedule = async () => {
-      const response = await fetch(url);
-      if (!response.ok) throw response;
-      const json = await response.json();
-      setSchedule(json);
+    const db = firebase.database().ref();
+    const handleData = (snap) => {
+      if (snap.val()) setSchedule(fixCourses(snap.val()));
     };
-    fetchSchedule();
+    db.on("value", handleData, (error) => alert(error));
+    return () => {
+      db.off("value", handleData);
+    };
   }, []);
 
   return (
